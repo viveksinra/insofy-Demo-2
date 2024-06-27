55f7b253d3cf4187843a2b78c685ff503e46e56d
viveksinra <vivek.kr.vks@gmail.com>
2024-06-27 13:01:01 +0530

    feat: Optimize image loading and update components on homepage

--- components/sections/Service6.js
+++ components/sections/Service6.js
@@ -1,7 +1,33 @@
 import Link from "next/link"
 
-
 export default function Service6() {
+    const services = [
+        {
+            icon: "flaticon-salary",
+            title: "Timely Payouts",
+            image: "/assets/images/service/other-ser-1.png",
+            description: "Say goodbye to long waits for payments on policy issuance! With us, get TIMELY PAYOUT",
+            linkText: "Start Earning Now",
+            link: "/service"
+        },
+        {
+            icon: "flaticon-management",
+            title: "Fully Digital Process",
+            image: "/assets/images/service/other-ser-2.jpg",
+            description: "No middlemen, no hidden charges - what you deserve is what you get! View & track your payments with your unique ID!",
+            linkText: "Sign Up",
+            link: "/service"
+        },
+        {
+            icon: "flaticon-business-trip",
+            title: "Dedicated Support",
+            image: "/assets/images/service/other-ser-3.jpg",
+            description: "Stuck somewhere? No worries! Our dedicated support team is there to help you-wherever, whenever!",
+            linkText: "Register With Us",
+            link: "/service"
+        }
+    ];
+
     return (
         <>
             <section className="service bg_light_2 bg_op_4" style={{ backgroundImage: 'url(assets/images/about-dot-bg.png)!important' }}>
@@ -24,108 +50,46 @@ export default function Service6() {
                     </div>
                     <div className="service_post position-relative">
                         <div className="row">
-                            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                                <div className="service_box type_six hover_1_get borenable color_two trans">
-                                    <div className="d-flex trans">
-                                        <div className="icon trans">
-                                            <i className=" flaticon-salary trans" /></div>
-                                        <div className="title_22">
-                                            <Link href="/service">Timely Payouts
+                            {services.map((service, index) => (
+                                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6" key={index}>
+                                    <div className="service_box type_six hover_1_get borenable color_two trans">
+                                        <div className="d-flex trans">
+                                            <div className="icon trans">
+                                                <i className={` ${service.icon} trans`} />
+                                            </div>
+                                            <div className="title_22">
+                                                <Link href={service.link}>{service.title}</Link>
+                                            </div>
+                                        </div>
+                                        <Link href={service.link} className="img_bx">
+                                            <div className="image trans hover_1">
+                                                <img src={service.image} className="img-fluid" alt="service" />
+                                                <div className="oh ho_1" />
+                                                <div className="oh ho_2" />
+                                                <div className="oh ho_3" />
+                                                <div className="oh ho_4" />
+                                            </div>
+                                        </Link>
+                                        <div className="content">
+                                            <p className="trans">{service.description}</p>
+                                            <Link href={service.link} className="theme_btn">
+                                                {service.linkText} 
+                                                <svg width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M1 11L11 1L1 11ZM11 1H3.5H11ZM11 1V8.5V1Z" fill="#696E77"></path>
+                                                    <path d="M1 11L11 1M11 1H3.5M11 1V8.5" stroke="#696E77" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                </svg>
                                             </Link>
                                         </div>
                                     </div>
-                                    <Link href="/service" className="img_bx">
-                                        <div className="image trans hover_1">
-                                            <img src="/assets/images/service/other-ser-2.jpg" className="img-fluid" alt="service" />
-                                            <div className="oh ho_1" />
-                                            <div className="oh ho_2" />
-                                            <div className="oh ho_3" />
-                                            <div className="oh ho_4" />
-                                        </div>
-                                    </Link>
-                                    <div className="content">
-                                        <p className="trans">
-                                        Say goodbye to long waits for payments on policy issuance! With us, get TIMELY PAYOUT</p>
-                                        <Link href="/service" className="theme_btn">
-                                            Start Earning Now <svg width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1 11L11 1L1 11ZM11 1H3.5H11ZM11 1V8.5V1Z" fill="#696E77">
-                                                </path>
-                                                <path d="M1 11L11 1M11 1H3.5M11 1V8.5" stroke="#696E77" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            </svg>
-                                        </Link>
-                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                                <div className="service_box type_six hover_1_get borenable color_two  trans">
-                                    <div className="d-flex trans">
-                                        <div className="icon trans">
-                                            <i className=" flaticon-management trans" /></div>
-                                        <div className="title_22">
-                                            <Link href="/service">Fully Digital Process</Link></div>
-                                    </div>
-                                    <Link href="/service" className="img_bx">
-                                        <div className="image trans hover_1">
-                                            <img src="/assets/images/service/other-ser-2.jpg" className="img-fluid" alt="service" />
-                                            <div className="oh ho_1" />
-                                            <div className="oh ho_2" />
-                                            <div className="oh ho_3" />
-                                            <div className="oh ho_4" />
-                                        </div>
-                                    </Link>
-                                    <div className="content">
-                                        <p className="trans">
-                                        No middlemen, no hidden charges - what you deserve is what you get! View & track your payments with your unique ID!</p>
-                                        <Link href="/service" className="theme_btn">
-                                            Sign Up
-                                            <svg width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1 11L11 1L1 11ZM11 1H3.5H11ZM11 1V8.5V1Z" fill="#696E77">
-                                                </path>
-                                                <path d="M1 11L11 1M11 1H3.5M11 1V8.5" stroke="#696E77" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            </svg>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                                <div className="service_box type_six hover_1_get borenable color_two  trans">
-                                    <div className="d-flex trans">
-                                        <div className="icon trans">
-                                            <i className=" flaticon-business-trip trans" /></div>
-                                        <div className="title_22">
-                                            <Link href="/service">Dedicated Support</Link></div>
-                                    </div>
-                                    <Link href="/service" className="img_bx">
-                                        <div className="image trans hover_1">
-                                            <img src="/assets/images/service/other-ser-1.jpg" className="img-fluid" alt="service" />
-                                            <div className="oh ho_1" />
-                                            <div className="oh ho_2" />
-                                            <div className="oh ho_3" />
-                                            <div className="oh ho_4" />
-                                        </div>
-                                    </Link>
-                                    <div className="content">
-                                        <p className="trans">
-                                        Stuck somewhere? No worries! Our dedicated support team is there to help you-wherever, whenever!</p>
-                                        <Link href="/service" className="theme_btn">
-                                            Register With Us <svg width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1 11L11 1L1 11ZM11 1H3.5H11ZM11 1V8.5V1Z" fill="#696E77">
-                                                </path>
-                                                <path d="M1 11L11 1M11 1H3.5M11 1V8.5" stroke="#696E77" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            </svg>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
-                
                 </div>
                 {/*-============spacing==========-*/}
                 <div className="pd_bottom_50" />
                 {/*-============spacing==========-*/}
             </section>
-
         </>
-    )
+    );
 }
