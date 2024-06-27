2ebf8251bac82b088a9f955fc695efbbf6c7bb8f
viveksinra <vivek.kr.vks@gmail.com>
2024-06-27 15:58:49 +0530

    feat: Add Banner2, Client2, Process2, Service3, Service4, Service5, and Testimonial2 components to homepage

--- components/sections/Process2.js
+++ components/sections/Process2.js
@@ -1,20 +1,41 @@
-import Link from "next/link"
-
+import Link from "next/link";
 
 export default function Process2() {
+    const steps = [
+        {
+            step: "Step 01",
+            title: "Partner with us",
+            description: "Fill up the simple one-time form and you are set to go with the best earnings & benefits on offer!",
+            imgSrc: "/assets/images/process/cus-pro1.png",
+        },
+        {
+            step: "Step 02",
+            title: "Expand your network & start earning",
+            description: "We're here to simplify the process for you as a leading insurance broker!",
+            imgSrc: "/assets/images/process/process-1-2.png",
+        },
+        {
+            step: "Step 03",
+            title: "Earn good commission",
+            description: "Make more money per insurance policy! Too good to be true? Speak to our experts to know how!",
+            imgSrc: "/assets/images/process/cus-commi.png",
+        },
+
+    ];
+
     return (
         <>
             <section className="process-section">
                 {/*-============spacing==========-*/}
-                <div className="pd_top_90" />
+                <div className="pd_top_60" />
                 {/*-============spacing==========-*/}
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-7 m-auto">
                             <div className="section_title text-center type_four">
-                                <h4 className="sm_title"> Working Process</h4>
+                                <h4 className="sm_title">Become A POSP Partner</h4>
                                 <div className="title_whole">
-                                    <h2 className="title"> Easy Way to Get Our Popular Insurance Services</h2>
+                                    <h2 className="title">How It Works</h2>
                                 </div>
                             </div>
                         </div>
@@ -23,101 +44,35 @@ export default function Process2() {
                     <div className="pd_bottom_40" />
                     {/*-============spacing==========-*/}
                     <div className="row">
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                            <div className="process_box type_two type_three color_two hover_1_get">
-                                <div className="image_box hover_1">
-                                    <img src="/assets/images/process/pro-h4-1-min.jpg" alt="img" className="img-fluid" />
-                                    <div className="oh ho_1" />
-                                    <div className="oh ho_2" />
-                                    <div className="oh ho_3" />
-                                    <div className="oh ho_4" />
-                                    <div className="icon trans" />
-                                </div>
-                                <div className="content_no">
-                                    <div className="con_top">
-                                        <p className="step">Step 01</p>
-                                        <div className="title_22">
-                                            <Link href="#">
-                                                Meet Our Agent </Link></div>
+                        {steps.map((step, index) => (
+                            <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12" key={index}>
+                                <div className="process_box type_two type_three color_two hover_1_get">
+                                    <div className="image_box hover_1">
+                                        <img src={step.imgSrc} alt="img" className="img-fluid" />
+                                        <div className="oh ho_1" />
+                                        <div className="oh ho_2" />
+                                        <div className="oh ho_3" />
+                                        <div className="oh ho_4" />
+                                        <div className="icon trans" />
                                     </div>
-                                    <p>Sed ut perspiciatis undey
-                                        este natus volutate</p>
+                                    <div className="content_no">
+                                        <div className="con_top">
+                                            <p className="step">{step.step}</p>
+                                            <div className="title_22">
+                                                <Link href="#">{step.title}</Link>
+                                            </div>
+                                        </div>
+                                        <p>{step.description}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                            <div className="process_box type_two type_three color_two hover_1_get">
-                                <div className="image_box hover_1">
-                                    <img src="/assets/images/process/pro-h4-2-min.jpg" alt="img" className="img-fluid" />
-                                    <div className="oh ho_1" />
-                                    <div className="oh ho_2" />
-                                    <div className="oh ho_3" />
-                                    <div className="oh ho_4" />
-                                    <div className="icon trans" />
-                                </div>
-                                <div className="content_no">
-                                    <div className="con_top">
-                                        <p className="step">Step 02</p>
-                                        <div className="title_22">
-                                            <Link href="#">
-                                                Select Insurance </Link></div>
-                                    </div>
-                                    <p>Sed ut perspiciatis undey
-                                        este natus volutate</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                            <div className="process_box type_two type_three color_two hover_1_get">
-                                <div className="image_box hover_1">
-                                    <img src="/assets/images/process/pro-h4-3-min.jpg" alt="img" className="img-fluid" />
-                                    <div className="oh ho_1" />
-                                    <div className="oh ho_2" />
-                                    <div className="oh ho_3" />
-                                    <div className="oh ho_4" />
-                                    <div className="icon trans" />
-                                </div>
-                                <div className="content_no">
-                                    <div className="con_top">
-                                        <p className="step">Step 03</p>
-                                        <div className="title_22">
-                                            <Link href="#">
-                                                Create Account </Link></div>
-                                    </div>
-                                    <p>Sed ut perspiciatis undey
-                                        este natus volutate</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                            <div className="process_box type_two type_three color_two hover_1_get">
-                                <div className="image_box hover_1">
-                                    <img src="/assets/images/process/pro-h4-4-min.jpg" alt="img" className="img-fluid" />
-                                    <div className="oh ho_1" />
-                                    <div className="oh ho_2" />
-                                    <div className="oh ho_3" />
-                                    <div className="oh ho_4" />
-                                    <div className="icon trans" />
-                                </div>
-                                <div className="content_no">
-                                    <div className="con_top">
-                                        <p className="step">Step 04</p>
-                                        <div className="title_22">
-                                            <Link href="#">
-                                                Get Insurance </Link></div>
-                                    </div>
-                                    <p>Sed ut perspiciatis undey
-                                        este natus volutate</p>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
                 {/*-============spacing==========-*/}
-                <div className="pd_bottom_70" />
+                <div className="pd_bottom_40" />
                 {/*-============spacing==========-*/}
             </section>
-
         </>
-    )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,47 @@
 import Layout from "@/components/layout/Layout"
 import About1 from "@/components/sections/About1"
 import Banner1 from "@/components/sections/Banner1"
+import Banner2 from "@/components/sections/Banner2"
 import Blog1 from "@/components/sections/Blog1"
 import Client1 from "@/components/sections/Client1"
+import Client2 from "@/components/sections/Client2"
 import Faq1 from "@/components/sections/Faq1"
 import Form1 from "@/components/sections/Form1"
 import Funfacts1 from "@/components/sections/Funfacts1"
 import Process1 from "@/components/sections/Process1"
+import Process2 from "@/components/sections/Process2"
+import Process3 from "@/components/sections/Process3"
 import Service1 from "@/components/sections/Service1"
 import Service2 from "@/components/sections/Service2"
+import Service3 from "@/components/sections/Service3"
+import Service4 from "@/components/sections/Service4"
+import Service5 from "@/components/sections/Service5"
+import Service6 from "@/components/sections/Service6"
 import Slider1 from "@/components/sections/Slider1"
 import Testimonial1 from "@/components/sections/Testimonial1"
+import Testimonial2 from "@/components/sections/Testimonial2"
+import Testimonial3 from "@/components/sections/Testimonial3"
+import Testimonial4 from "@/components/sections/Testimonial4"
+import Testimonial5 from "@/components/sections/Testimonial5"
 export default function Home1() {
 
     return (
         <>
             <Layout headerStyle={1} footerStyle={1}>
-            <Banner1 />
-                <Service1 />
-                <About1 />
-                <Service2 />
+            <Banner2 />
+       
+                <Service6 />
+                <Process2 />
+                <Client2 />
+
+
+
+
+                
+                {/* <Funfacts1 />
+                <Form1 /> */}
+                <Testimonial2 />
                 <Faq1 />
-                <Funfacts1 />
-                <Form1 />
-                <Process1 />
-                <Testimonial1 />
-                <Client1 />
                 <Blog1 />
             </Layout>
         </>
