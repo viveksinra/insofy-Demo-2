7505e4a77e8f210493f496cb46bae76ef02426ce
viveksinra <vivek.kr.vks@gmail.com>
2024-06-27 16:38:44 +0530

    fix

---
+++ components/sections/Banner3.js
@@ -0,0 +1,60 @@
+
+
+export default function Banner3() {
+    return (
+        <>
+            <section className="single_banner style_one">
+                
+                <div className="content_box">
+                    <div className="large-container">
+                        <div className="row align-items-center">
+                        <div className="col-lg-6">
+                                <div className="image">
+                                    <img src="/assets/images/slider/newBan.png" className="img-fluid" alt="img" />
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="section_title type_one">
+                                    <div className="title_whole">
+                                        <h2 className="title">
+                                        Partner With Us,
+                                        Grow Your Insurance
+                                        Business Today
+                                        </h2>
+                                    </div>
+                               
+                                </div>
+                                <div className="newsteller_simple">
+                                    <div className="input_group">
+                                        <form method="post">
+                                            <input type="number" name="MOBILE" placeholder="Your mobile number" required />
+                                            <input type="submit" value="Sign up" />
+                                        </form>
+                                    </div>
+                                </div>
+                                <div className="review_box d-flex align-items-center">
+                                    <div className="rimage">
+                                        <img src="/assets/images/slider/review-girls.png" className="img-fluid" alt="img" />
+                                    </div>
+                                    <div className="section_title type_one">
+                                        <div className="title_whole">
+                                            <h2 className="title"> Excellent 12,534+ reviews</h2>
+                                        </div>
+                                        <p>4.8 of 5 <small><i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /></small></p>
+                                    </div>
+                                </div>
+                            </div>
+                          
+                        </div>
+                    </div>
+                </div>
+                <div className="section_title abso type_one">
+                    <div className="title_whole">
+                        <h2 className="title"> INSURANCE</h2>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
